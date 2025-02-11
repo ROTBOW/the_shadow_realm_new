df871d80ca9b689f7c1a2950265179bb046df16d
Josiah Leon <sjosiahleon@gmail.com>
2025-02-11 10:07:11 -0800

    Basic navbar setup added fonts

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -3,18 +3,14 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-      <div className="flex justify-between bg-black">
-        <div className='flex'>
-          <Link href={'/'} className="text-3xl ">The Shadow Realm</Link>
-              <div className="flex w-1/3">
-                  <Link href="/">Home</Link>
-                  <Link href="/cards">All Cards</Link>
-                  <Link href="random">Random</Link>
-                  {/* <Link href="user-deck">Deck Builder</Link> */}
-              </div>
-        </div>
-              <div className='w-1/2 bg-white'>
-              </div>
+      <div className='flex items-center w-full bg-gray-800 h-12'>
+        <Link href={'/'} className="text-3xl ml-3 font-tradewinds text-center">The Shadow Realm</Link>
+            <div className="flex w-1/3 items-center justify-around font-farsan text-2xl bold">
+                <Link href="/">Home</Link>
+                <Link href="/cards">All Cards</Link>
+                <Link href="random">Random</Link>
+                {/* <Link href="user-deck">Deck Builder</Link> */}
+            </div>
       </div>
     );
   }
